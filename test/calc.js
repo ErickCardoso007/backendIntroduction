90f10fefeff3c769091b9dbc0cd3312932bce10a
Erick Cardoso <cyber666king@gmail.com>
2022-06-13 06:33:56 -0300

    feat:(test) refatora código utilizado o sistema de modulos do node.js

---
+++ test/calc.js
@@ -0,0 +1,10 @@
+const somaFunc = require("./soma")
+const subFunc = require("./sub")
+const divFunc = require("./div")
+const multiFunc = require("./multi")
+
+console.log(somaFunc(2, 4))
+console.log(subFunc(10, 2))
+console.log(divFunc(112, 4))
+console.log(multiFunc(333, 21))
+
